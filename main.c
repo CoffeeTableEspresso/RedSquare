f0739b5abc89e752d5a8975520ef724533c4feda
CoffeeTableEspresso <coffeetableespresso@gmail.com>
2020-05-18 01:26:41 -0700

    got it working with YASL

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+#include <yasl/yasl.h>
+
 #include <stdio.h>
 #include <stdbool.h>
 
 #include "player.h"
 #include "constants.h"
+#include "vect.h"
+
+VECT_DECL(SDL_Rect)
 
 #define ASSET(x) ("assets/" #x)
 
@@ -58,6 +65,11 @@ bool winit(struct Window_State *state) {
 		return false;
 	}
 
+	if (TTF_Init() == -1) {
+		fprintf(stderr, "SDL_ttf could not be initialized: %s\n", TTF_GetError());
+		return false;
+	}
+
 	state->screen = SDL_GetWindowSurface(state->window);
 	return true;
 }
@@ -100,13 +112,56 @@ void wclose(struct Window_State *state) {
 	SDL_DestroyRenderer(state->renderer);
 	SDL_DestroyWindow(state->window);
 
+	TTF_Quit();
 	IMG_Quit();
 	SDL_Quit();
 }
 
+bool collide(SDL_Rect *a, SDL_Rect *b) {
+	return (a->x) < (b->x + b->w) &&
+		(a->x + a->w) > (b->x) &&
+		(a->y + a->h) > (b->y) &&
+		(a->y) < (b->y + b->h);
+}
+
+bool collide_any(Vect(SDL_Rect) *v, SDL_Rect *r) {
+	for (size_t i = 0; i < v->count; i++) {
+		if (collide(&v->items[i], r)) return true;
+	}
+	return false;
+}
+
+static struct Player player;
+static Vect(SDL_Rect) enemies;
+static SDL_Rect goal;
+
+int AddPlayer(struct YASL_State *S) {
+	yasl_int y = YASL_popint(S);
+	yasl_int x = YASL_popint(S);
+	player.rect.x = x;
+	player.rect.y = y;
+	return YASL_SUCCESS;
+}
+
+int AddEnemy(struct YASL_State *S) {
+	yasl_int h = YASL_popint(S);
+	yasl_int w = YASL_popint(S);
+	yasl_int y = YASL_popint(S);
+	yasl_int x = YASL_popint(S);
+	v_push(SDL_Rect)(&enemies, (SDL_Rect){ x, y, w, h});
+	return YASL_SUCCESS;
+}
+
+int AddGoal(struct YASL_State *S) {
+	yasl_int y = YASL_popint(S);
+	yasl_int x = YASL_popint(S);
+	goal.x = x;
+	goal.y = y;
+	return YASL_SUCCESS;
+}
+
 int main(int argc, char *argv[]) {
 	struct Window_State state;
-
 	winit(&state);
 
 	load_media(&state);
@@ -114,33 +169,120 @@ int main(int argc, char *argv[]) {
 	bool quit = false;
 	SDL_Event event_handler;
 
-	const int step_size = 1;
-
-	struct Player player = {
-		{
-			SCREEN_WIDTH / 2,
-			SCREEN_HEIGHT / 2,
-			SCREEN_WIDTH / 25,
-			SCREEN_WIDTH / 25
-		},
-		0,
-		0
-	};
-
 	while (!quit) {
-		printf("(x,y): (%d, %d)\n", player.rect.x, player.rect.y);
-		while (SDL_PollEvent(&event_handler) != 0) {
-			if (event_handler.type == SDL_QUIT) quit = true;
-			pl_handle_input(&player, &event_handler);
+		bool dead = false;
+		enemies = v_init(SDL_Rect)();
+		player.rect = (SDL_Rect) {0, 0, SCREEN_WIDTH / 25, SCREEN_WIDTH / 25};
+		goal = (SDL_Rect) {0, 0, SCREEN_WIDTH / 50, SCREEN_WIDTH / 50};
+
+		struct YASL_State *S = YASL_newstate("level.yasl");
+		if (!S) {
+			fprintf(stderr, "Failed to load level\n");
 		}
 
-		pl_update(&player);
+		YASL_declglobal(S, "SCREEN_WIDTH");
+		YASL_pushinteger(S, SCREEN_WIDTH);
+		YASL_setglobal(S, "SCREEN_WIDTH");
 
-		SDL_SetRenderDrawColor(state.renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-		SDL_RenderClear(state.renderer);
-		SDL_SetRenderDrawColor(state.renderer, 0xFF, 0x00, 0x00, 0xFF);
-		SDL_RenderFillRect(state.renderer, &player.rect);
-		SDL_RenderPresent(state.renderer);
+		YASL_declglobal(S, "SCREEN_HEIGHT");
+		YASL_pushinteger(S, SCREEN_HEIGHT);
+		YASL_setglobal(S, "SCREEN_HEIGHT");
+
+		YASL_declglobal(S, "AddPlayer");
+		YASL_pushcfunction(S, AddPlayer, 2);
+		YASL_setglobal(S, "AddPlayer");
+
+		YASL_declglobal(S, "AddEnemy");
+		YASL_pushcfunction(S, AddEnemy, 4);
+		YASL_setglobal(S, "AddEnemy");
+
+		YASL_declglobal(S, "AddGoal");
+		YASL_pushcfunction(S, AddGoal, 2);
+		YASL_setglobal(S, "AddGoal");
+
+		YASL_execute(S);
+
+		while (!dead && !quit) {
+			while (SDL_PollEvent(&event_handler) != 0) {
+				if (event_handler.type == SDL_QUIT) quit = true;
+				pl_handle_input(&player, &event_handler);
+			}
+
+			pl_update(&player);
+
+			SDL_SetRenderDrawColor(state.renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+			SDL_RenderClear(state.renderer);
+			SDL_SetRenderDrawColor(state.renderer, 0x00, 0x00, 0x00, 0xFF);
+			for (size_t i = 0; i < enemies.count; i++) {
+				SDL_RenderFillRect(state.renderer, &enemies.items[i]);
+			}
+			SDL_SetRenderDrawColor(state.renderer, 0x00, 0xFF, 0x00, 0xFF);
+			SDL_RenderFillRect(state.renderer, &goal);
+			SDL_SetRenderDrawColor(state.renderer, 0xFF, 0x00, 0x00, 0xFF);
+			SDL_RenderFillRect(state.renderer, &player.rect);
+			SDL_RenderPresent(state.renderer);
+
+			if (collide_any(&enemies, &player.rect)) {
+				// TODO move this set up to init.
+				SDL_SetRenderDrawColor(state.renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+				SDL_RenderClear(state.renderer);
+				SDL_SetRenderDrawColor(state.renderer, 0x88, 0x00, 0x00, 0xFF);
+				SDL_RenderFillRect(state.renderer, &player.rect);
+				SDL_Color color = {0xFF, 0x00, 0x00};
+				TTF_Font *font = TTF_OpenFont("fonts/lazy.ttf", 20);
+				SDL_Surface *text = TTF_RenderText_Solid(font, "YOU DIED :(", color);
+				SDL_Texture *texture = SDL_CreateTextureFromSurface(state.renderer, text);
+				SDL_FreeSurface(text);
+				TTF_CloseFont(font);
+				while (!quit && !dead) {
+					while (SDL_PollEvent(&event_handler) != 0) {
+						if (event_handler.type == SDL_QUIT) quit = true;
+						if (event_handler.type == SDL_KEYDOWN &&
+						    event_handler.key.repeat == 0 &&
+						    event_handler.key.keysym.sym == SDLK_SPACE)
+							dead = true;
+						pl_handle_input(&player, &event_handler);
+					}
+					SDL_SetRenderDrawColor(state.renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+					SDL_RenderClear(state.renderer);
+					SDL_SetRenderDrawColor(state.renderer, 0x88, 0x00, 0x00, 0xFF);
+					SDL_RenderFillRect(state.renderer, &player.rect);
+					SDL_RenderCopy(state.renderer, texture, NULL, NULL);
+					SDL_RenderPresent(state.renderer);
+				}
+			} else if (collide(&goal, &player.rect)) {
+				// TODO move this set up to init.
+				SDL_SetRenderDrawColor(state.renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+				SDL_RenderClear(state.renderer);
+				SDL_SetRenderDrawColor(state.renderer, 0x00, 0x88, 0x00, 0xFF);
+				SDL_RenderFillRect(state.renderer, &player.rect);
+				SDL_Color color = {0x00, 0xFF, 0x00};
+				TTF_Font *font = TTF_OpenFont("fonts/lazy.ttf", 20);
+				SDL_Surface *text = TTF_RenderText_Solid(font, "YOU WIN :)", color);
+				SDL_Texture *texture = SDL_CreateTextureFromSurface(state.renderer, text);
+				SDL_FreeSurface(text);
+				TTF_CloseFont(font);
+				while (!quit && !dead) {
+					while (SDL_PollEvent(&event_handler) != 0) {
+						if (event_handler.type == SDL_QUIT) quit = true;
+						if (event_handler.type == SDL_KEYDOWN &&
+						    event_handler.key.repeat == 0 &&
+						    event_handler.key.keysym.sym == SDLK_SPACE)
+							dead = true;
+						pl_handle_input(&player, &event_handler);
+					}
+					SDL_SetRenderDrawColor(state.renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+					SDL_RenderClear(state.renderer);
+					SDL_SetRenderDrawColor(state.renderer, 0x00, 0x88, 0x00, 0xFF);
+					SDL_RenderFillRect(state.renderer, &player.rect);
+					SDL_RenderCopy(state.renderer, texture, NULL, NULL);
+					SDL_RenderPresent(state.renderer);
+				}
+			}
+		}
+
+		YASL_delstate(S);
+		v_cleanup(SDL_Rect)(&enemies);
 	}
 
 	wclose(&state);
---
+++ vect.c
@@ -0,0 +1,42 @@
+//
+// Created by thiabaud on 17/05/20.
+//
+
+#include "vect.h"
+#include <SDL2/SDL.h>
+
+#define VECT_INIT_CAP 8
+
+#define VECT_INSTANTIATE(T)\
+struct VECT_NAME(T) {\
+	size_t count, cap;\
+	T *items;\
+};\
+\
+Vect(T) v_init(T)(void) {\
+	return (Vect(T)){\
+		0, VECT_INIT_CAP, malloc(VECT_INIT_CAP * sizeof(T))\
+	};\
+};\
+\
+void v_cleanup(T)(Vect(T) *v) {\
+	free(v->items);\
+}\
+\
+void v_push(T)(Vect(T) *v, T t) {\
+	if (v->count == v->cap) {\
+		v->cap *= 2;\
+		v->items = realloc(v->items, v->cap * sizeof(T));\
+	}\
+	v->items[v->count++] = t;\
+}\
+\
+T v_pop(T)(Vect(T) *v) {\
+	return v->items[--v->count];\
+}\
+\
+T v_get(T)(Vect(T) *v, size_t idx) {\
+	return v->items[idx];\
+}
+
+VECT_INSTANTIATE(SDL_Rect)
